ace848e799ca1da05e89936a13a8c9c3ae348d7d
Villeky <ville.kylkilahti@student.lut.fi>
2024-04-13 12:56:30 +0300

    Summeri aktivointi

--- mega_SPI_master_basics.c
+++ mega_SPI_master_basics.c
@@ -7,64 +7,75 @@
 
 #define F_CPU 16000000UL
 
+#include <stdlib.h>
 #include <stdbool.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "lcd.h" // lcd header file made by Peter Fleury
 
-volatile bool aktivoitu = false;
+volatile bool activated = false;
 volatile bool code_correct = false;
 
 int main(void){
-	/* initialize display, cursor off */
+	/* Summerin ulostulo asetus */
+	DDRB |=  (1 << PB0); // 0b00000110  ARDUINO PORTTI 8
+	/* Näytön alustus */
 	lcd_init(LCD_DISP_ON);
-	
-	
 	int16_t time_left = 10;
-	char time_left_array[16]; // 16-bit array, assumes that the int given is 16-bits
-	lcd_clrscr();
-	lcd_puts("Security system");
-	lcd_gotoxy(0,1);
-	lcd_puts("Active!");
-	_delay_ms(5000);
-	/* clear display */
-	lcd_clrscr();
-	// Odota että sensori aktivoituu -> aktivoitu = true;
+	char time_left_array[16]; // 16-bit taulukko
+	int16_t code_entered = 0;
+	int16_t correct_code = 1234;
 
-	if (aktivoitu == true){
-	
 	while(1){
-	// Koodin tarkistus -> code_correct = true;
-	// Hälyytyksen pysäytys
-
-	if (code_correct == true){
-	lcd_clrscr();
-	lcd_puts("Alarm");
-	lcd_gotoxy(0,1);
-	lcd_puts("Deactivated!");
-	break;
-	}
-
-	itoa(time_left, time_left_array, 10);
-	lcd_clrscr();
-	lcd_puts("Enter code!");
-
-	lcd_gotoxy(0,1);
-	lcd_puts("Time left: ");
-	lcd_puts(time_left_array);
-	
-	if(time_left == 0){
+	if(activated == true){
+		// Koodin tarkistus -> code_correct = true;
+		// Hälyytyksen pysäytys
+		if (code_entered == correct_code){
+			lcd_clrscr();
+			lcd_puts("Alarm");
+			lcd_gotoxy(0,1);
+			lcd_puts("Deactivated!");
+			_delay_ms(2000);
+			activated = false;
+			time_left = 10;
+			}
+		itoa(time_left, time_left_array, 10);
 		lcd_clrscr();
-		lcd_puts("Alarm activated!");
-		//Summeri päälle
-	}
+		lcd_puts("Enter code!");
+		lcd_gotoxy(0,1);
+		lcd_puts("Time left: ");
+		lcd_puts(time_left_array);
+		
+		if(time_left == 0){
+			lcd_clrscr();
+			lcd_puts("Alarm activated!");
+			/* Summeri päälle */
+			// (Välkkyvä led)
+			for(int i=0; i<10;i++){
+				_delay_ms(500);
+				PORTB |=  (1 << PB0);
+				_delay_ms(500);
+				PORTB &= ~(1 << PB0);
+			}
+			activated = false;
+			time_left = 10;
+			PORTB &= ~(1 << PB0);
+			}
+		else{
+			time_left = time_left-1;
+			}
+		}
 	else{
-	time_left = time_left-1;
-	}
-	}
+		lcd_clrscr();
+		lcd_puts("Security system");
+		lcd_gotoxy(0,1);
+		lcd_puts("Active!");
+		_delay_ms(5000);
+		// Odota että sensori aktivoituu -> aktivoitu = true;
+		activated = true;
+		}
+
 	_delay_ms(1000);
 	}
-}
-	
 	return 0;
 }
